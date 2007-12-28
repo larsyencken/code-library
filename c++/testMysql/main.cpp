d42198d69308b4f0f13ec7a4ae3440410b7f944a
Lars Yencken <lljy@csse.unimelb.edu.au>
2007-12-28 20:39:09 +1100

    Added a file to compile to test MySQL headers.

---
+++ c++/testMysql/main.cpp
@@ -0,0 +1,19 @@
+//--------------------------------------------------------------------------//
+// main.cpp
+// Lars Yencken <lars.yencken@gmail.com>
+// vim: ts=4 sw=4 sts=4 expandtab:
+// Sun  2 Dec 2007 18:21:32 CET
+//--------------------------------------------------------------------------//
+
+#include <iostream>
+#include <sys/types.h>
+#include "mysql.h"
+
+//--------------------------------------------------------------------------//
+// main():
+//
+int main(int argc, char *argv[])
+{
+    std::cout << "Working!" << std::endl;
+    return 0;
+}
