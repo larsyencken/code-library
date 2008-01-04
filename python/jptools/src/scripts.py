9c7566d278e3ab4807402d64155b8b0bf0202cba
Lars Yencken <lljy@csse.unimelb.edu.au>
2008-01-04 15:18:42 +1100

    Added an old version of jptools written in pure python.

---
+++ python/jptools/src/scripts.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------#
+# __init__.py
+# Lars Yencken <lars.yencken@gmail.com>
+# vim: ts=4 sw=4 sts=4 et tw=78:
+# Sun May 15 23:12:04 EST 2005
+#
+#----------------------------------------------------------------------------#
+
+"""
+This module is responsible for Japanese script/encoding specific methods,
+especially determining the script type of an entry. It is thus the only
+module which requires a utf8 encoding for the additional Japanese characters.
+"""
+
+#----------------------------------------------------------------------------#
+
+import enum
+
+#----------------------------------------------------------------------------#
+# CONSTANTS
+#----------------------------------------------------------------------------#
+
+Script = enum.Enum(u'Hiragana', u'Katakana', u'Kanji', u'Ascii',
+        u'FullAscii', u'Unknown')
+
+_knownBands = {
+        Script.Ascii:       (u'\u0021', u'\u00ff'),
+        Script.Hiragana:	(u'\u3041', u'\u3096'),
+        Script.Katakana:	(u'\u30a1', u'\u30f6'),
+        Script.Kanji:	    (u'\u4e00', u'\u9fa5'),
+        Script.FullAscii:	(u'\uff01', u'\uff5f'),
+    }
+
+_interKanaDistance = 96
+
+#----------------------------------------------------------------------------#
+
+class ScriptMapping:
+    """
+    A mapping function between two scripts. We assume that the given scripts
+    are different versions of the same characters, and further that they are
+    aligned at the start and end points stored.
+    """
+    def __init__(self, fromScript, toScript):
+        """
+        Constructor, initializes script conversion.
+        
+        @param fromScript: The script to convert from.
+        @type fromScript: Script
+        @param toScript: The script to convert to.
+        @type toScript: Script
+        """
+        self.fromStart, self.fromEnd = _knownBands[fromScript]
+        self.toStart, self.toEnd = _knownBands[toScript]
+        self.ordDiff = ord(self.toStart) - ord(self.fromStart)
+        assert (ord(self.fromEnd) - ord(self.fromStart)) <= \
+                (ord(self.toEnd) - ord(self.toStart))
+        return
+
+    def __call__(self, jString):
+        """
+        Converts any matching characters in the given string between scripts.
+        Any characters which don't match the input script are passed through
+        unchanged.
+        """
+        result = []
+        for char in jString:
+            if self.fromStart <= char <= self.fromEnd:
+                result.append(unichr(ord(char) + self.ordDiff))
+            else:
+                result.append(char)
+
+        return u''.join(result)
+
+toHiragana = ScriptMapping(Script.Katakana, Script.Hiragana)
+toKatakana = ScriptMapping(Script.Hiragana, Script.Katakana)
+
+#----------------------------------------------------------------------------#
+
+_mappedChars = {
+            u'\u3000':  u'\u0020',
+            u'\u3001':  u'\u002c',
+            u'\u3002':  u'\u002e',
+        }
+
+_toAscii = ScriptMapping(Script.FullAscii, Script.Ascii)
+
+def normalizeAscii(jString):
+    """
+    Normalize a double-width ascii string, converting all characters to
+    their single-width counterparts.
+
+    @param jString: The string to convert.
+    @type jString: unicode
+    @return: The converted string.
+    """
+    result = []
+    for char in jString:
+        result.append(_mappedChars.get(char, char))
+
+    return _toAscii(u''.join(result))
+
+#----------------------------------------------------------------------------#
+
+def getScript(script):
+    """
+    Returns a string containing all charcters in the given script.
+    """
+
+    scriptStarts, scriptEnds = _knownBands[script]
+
+    output = []
+    for i in xrange(ord(scriptStarts), ord(scriptEnds) + 1):
+        output.append(unichr(i))
+
+    return ''.join(output)
+
+#----------------------------------------------------------------------------#
+
+def compareKana(jStringA, jStringB):
+    """
+    Compares two kana strings in a script-independent manner.
+
+        >>> ru_h = unicode('る', 'utf8')
+        >>> ka_h = unicode('か', 'utf8')
+        >>> ka_k = unicode('カ', 'utf8')
+        >>> compareKana(ru_h, ka_k) == cmp(ru_h, ka_h)
+        True
+
+    @type jStringA: unicode
+    @type jStringB: unicode
+    """
+    return cmp(toKatakana(jStringA), toKatakana(jStringB))
+
+#----------------------------------------------------------------------------#
+
+def containsScript(script, jString):
+    """
+    Returns True if the given script is within the string, False
+    otherwise.
+    
+        >>> woof = u'woof'
+        >>> containsScript(Script.Ascii, woof)
+        True
+        >>> containsScript(Script.Kanji, woof)
+        False
+
+    @param script: The script to search for.
+    @type script: Script
+    @param jString: The string to search within.
+    @type jString: unicode
+    """
+    return script in scriptTypes(jString)
+
+#----------------------------------------------------------------------------#
+
+def scriptType(char):
+    """
+    Determine the type of script contained in the given character. Script
+    types are expressed using the Script enum.
+
+        >>> woof = u'woof'
+        >>> scriptTypes(woof)
+        set([Ascii])
+
+        >>> beijing = unicode('北京', 'utf8')
+        >>> scriptTypes(beijing)
+        set([Kanji])
+
+        >>> ru = unicode('る', 'utf8')
+        >>> scriptTypes(ru)
+        set([Hiragana])
+
+    @param char: The character to examine.
+    @type char: unicode
+    @return: The script type.   
+    """
+    # Normalize/typecheck our input.
+    char = unicode(char)[0] 
+
+    for script, (startBand, endBand) in _knownBands.iteritems():
+        if startBand <= char <= endBand:
+            return script
+    else:
+        return Script.Unknown
+
+#----------------------------------------------------------------------------#
+
+def scriptBoundaries(jString):
+    """
+    Determines where the script boundaries are in the given string.
+        
+        >>> taberu = unicode('食べる', 'utf8')
+        >>> scriptBoundaries(taberu) == (taberu[0], taberu[1:])
+        True
+
+    @param jString: The string of Japanese to segment.
+    @type jString: string
+    @return: A tuple of script-contiguous blocks
+    """
+    assert type(jString) == unicode
+    segments = ()
+    currentSegType = scriptType(jString[0])
+    currentSeg = jString[0]
+    for char in jString[1:]:
+        if scriptType(char) == currentSegType or char == u'ー':
+            currentSeg += char
+        else:
+            segments += currentSeg,
+
+            currentSegType = scriptType(char)
+            currentSeg = char
+    else:
+        if currentSeg:
+            segments += currentSeg,
+    
+    return segments
+
+#----------------------------------------------------------------------------#
+
+def scriptTypes(jString):
+    """
+    Returns a set of the script types in the given string.
+
+        >>> woof = u'woof'
+        >>> scriptTypes(woof)
+        set([Ascii])
+
+        >>> beijing = unicode('北京', 'utf8')
+        >>> scriptTypes(beijing)
+        set([Kanji])
+
+        >>> taberu = unicode('食べる', 'utf8')
+        >>> scriptTypes(taberu)
+        set([Hiragana, Kanji])
+    """
+    return set(map(scriptType, jString))
+
+#----------------------------------------------------------------------------#
+
+def uniqueKanji(jString):
+    """
+    Returns the set of all unique kanji found in the given string.
+
+        >>> taberu = unicode('食べる', 'utf8')
+        >>> uniqueKanji(taberu) == set([taberu[0]])
+        True
+    """
+    kanjiSet = set(jString)
+    for char in list(kanjiSet):
+        if scriptType(char) != Script.Kanji:
+            kanjiSet.remove(char)
+
+    return kanjiSet
+
+#----------------------------------------------------------------------------#
---
+++ python/jptools/src/sequences.py
@@ -0,0 +1,481 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------#
+# sequences.py
+# Lars Yencken <lars.yencken@gmail.com>
+# vim: ts=4 sw=4 sts=4 et tw=78:
+# Mon Jun 25 14:53:16 2007
+#
+#----------------------------------------------------------------------------#
+
+"""
+This module contains methods for dealing with sequences. In particular, it
+contains iterators for interesting sequences and methods for sequence/tuple
+transformations.
+"""
+
+#----------------------------------------------------------------------------#
+
+from itertools import izip, chain
+
+#----------------------------------------------------------------------------#
+
+def zipWith(method, listA, listB):
+    """
+    Applies a two-argument method to successive pairs of items taken from
+    the two input lists. Returns the list of resulting items.
+
+    @param method: The two-argument method to run over element pairs.
+    @param listA: The first providing the first tuple element for each
+        pair.
+    @param listB: The first providing the second tuple element for each
+        pair.
+    """
+    return list(izipWith(method, listA, listB))
+
+#----------------------------------------------------------------------------#
+
+def izipWith(method, listA, listB):
+    """
+    As with zipWith(), but provides an iterator. 
+    """
+    for itemA, itemB in izip(listA, listB):
+        yield method(itemA, itemB)
+    
+    return
+
+#----------------------------------------------------------------------------#
+
+def flatten(objectSeq):
+    """
+    Flattens all levels of lists from the input, preserving original order
+    of items.
+    """
+    return list(iflatten(objectSeq))
+
+#----------------------------------------------------------------------------#
+
+def iflatten(objectSeq):
+    """
+    Returns an iterator over all the objects in the flattened sequence. The
+    original order is preserved.
+
+    @param objectSeq: A sequence of objects which themselves may be sequences
+    (and their children, etc.).
+    @type objectSeq: sequence
+    @return: An iterator over all items in all subsequences.
+    """
+    objectSeq = iter(objectSeq)
+
+    while True:
+        try:
+            item = objectSeq.next()
+
+            try:
+                # Try to coerce this item into a sequence.
+                subSeq = iter(item)
+            except TypeError:
+                # Failed, it's not a sequence of objects.
+                yield item
+            else:
+                # Succeeded, it's a subsequence, so iterate into it.
+                objectSeq = chain(iter(item), objectSeq)
+
+        except StopIteration:
+            break
+
+    return
+
+#----------------------------------------------------------------------------#
+
+def unzip(pairList):
+    """
+    The reverse of the zip() method. Given a sequence of tuples of the
+    same size, extracts two or more lists.
+
+        >>> unzip([(1,2), (3,4), (5,6)])
+        ([1, 3, 5], [2, 4, 6])
+
+    @param pairList: The sequence of tuples to draw the input from.
+    """
+    assert pairList
+    numLists = len(pairList[0])
+
+    newLists = []
+    for i in range(numLists):
+        newLists.append([])
+
+    for pair in pairList:
+        for i in range(numLists):
+            newLists[i].append(pair[i])
+    
+    return tuple(newLists)
+
+#----------------------------------------------------------------------------#
+
+def thread(inputList, tupleSize=2):
+    """
+    Turns a list of items into a list of tuples by simply placing
+    sequential items into tuples:
+
+        >>> thread([1, 2, 3, 4, 5])
+        [(1, 2), (3, 4)]
+
+    Notice that the last element might be discarded, in a similar manner
+    as when using zip.
+    """
+    return list(ithread(inputList, tupleSize))
+
+#----------------------------------------------------------------------------#
+
+def ithread(inputSeq, tupleSize=2):
+    """
+    Identical to thread() but returns an interator over pairs.
+
+        >>> list(ithread([1, 2, 3, 4, 5]))
+        [(1, 2), (3, 4)]
+    """
+    inputSeq = iter(inputSeq)
+
+    listRange = range(tupleSize)
+    while True:
+        output = []
+        try:
+            for i in range(tupleSize):
+                output.append(inputSeq.next())
+        except StopIteration:
+            break
+
+        else:
+            yield tuple(output)
+
+    return
+
+#----------------------------------------------------------------------------#
+
+def iunthread(inputTuples):
+    """
+    Turns a list of tuples into a flat list:
+
+        >>> list(iunthread([(1, 2), (3, 4)]))
+        [1, 2, 3, 4]
+
+    @param inputTuples: A list of tuples
+    @type inputTuples: list(tuple)
+    @return: A flat list
+    """
+    for itemTuple in inputTuples:
+        for item in itemTuple:
+            yield item
+
+    return
+
+#----------------------------------------------------------------------------#
+
+def unthread(inputTuples):
+    """
+    Turns a list of tuples into a flat list:
+
+        >>> unthread([(1, 2), (3, 4)])
+        [1, 2, 3, 4]
+
+    @param inputTuples: A list of tuples
+    @type inputTuples: list(tuple)
+    @return: A flat list
+    """
+    return list(iunthread(inputTuples))
+
+#----------------------------------------------------------------------------#
+
+def repeat(n, item):
+    """
+    Creates an iterator which provides n references to item. Note that they
+    are references, not copies.
+
+        >>> list(repeat(10, 1)) == [1]*10
+        True
+
+    @param n: The number of copies to yield in total.
+    @type n: int
+    @param item: The item to repeat.
+    @type item: object
+    """
+    for i in xrange(n):
+        yield item
+    return
+
+#----------------------------------------------------------------------------#
+
+def repeateIndef(item):
+    """Returns an iterator which repeats the item indefinitely."""
+    while 1:
+        yield item
+
+    return
+
+#----------------------------------------------------------------------------#
+
+def succession(itemList):
+    """
+    Returns an iterator which builds up the item list point by point.
+        
+        >>> list(succession([1, 2, 3]))
+        [[1], [1, 2], [1, 2, 3]]
+    """
+    for i in xrange(1, len(itemList)+1):
+        yield itemList[:i]
+    
+    return
+
+#----------------------------------------------------------------------------#
+
+def iwindow(itemList, windowSize=2, preBlanks=False):
+    """
+    Returns a sliding window iterator over the given list.
+
+        >>> window([1, 2, 3, 4, 5])
+        [(1, 2), (2, 3), (3, 4), (4, 5)]
+        >>> window([1, 2, 3, 4], 3)
+        [(1, 2, 3), (2, 3, 4)]
+        >>> window([1, 2, 3, 4], 3, preBlanks=True)
+        [(None, None, 1), (None, 1, 2), (1, 2, 3), (2, 3, 4)]
+
+    @param itemList: The list to iterate over.
+    @param windowSize: The number of elements in each window frame.
+    """
+    if preBlanks:
+        if windowSize < 2:
+            raise ValueError, "Need a window size >= 2 to use pre-blanks"
+        itemList = [None]*(windowSize-1) + itemList
+
+    for endWindow in range(windowSize, len(itemList)+1):
+        yield tuple(itemList[endWindow-windowSize:endWindow])
+
+    return 
+
+def window(itemList, windowSize=2, preBlanks=False):
+    """Identical to iwindow(), but returns a list."""
+    return list(iwindow(itemList, windowSize, preBlanks))
+
+#----------------------------------------------------------------------------#
+
+def groupByLambda(func, items):
+    """Performs a grouping of the items by lambda value."""
+    import warn
+    warn.warn(u'groupByLambda() is deprecated')
+    groups = {}
+    for item in items:
+        keyValue = func(item)
+
+        itemList = groups.get(keyValue, [])
+        itemList.append(item)
+        groups[keyValue] = itemList
+    
+    return groups
+
+#----------------------------------------------------------------------------#
+
+def multiDict(inputPairs):
+    """
+    Similar to casting pairs to a dictionary, except that repeated pairs
+    are allowed.
+    
+        >>> dict( [('a', 1), ('b', 2), ('a', 3)] )
+        {'a': 3, 'b': 2}
+        >>> multiDict( [('a', 1), ('b', 2), ('a', 3)] )
+        {'a': [1, 3], 'b': [2]}
+
+    @param inputPairs: A list of (key, value) pairs.
+    @return: A dictionary mapping keys to lists of values.
+    """
+    outputDict = {}
+
+    for key, value in inputPairs:
+        existingValues = outputDict.get(key, [])
+        existingValues.append(value)
+        outputDict[key] = existingValues
+    
+    return outputDict
+
+#----------------------------------------------------------------------------#
+
+def procmap(procedure, itemList):
+    """
+    Like map(), but where the method being applied has no return value. In
+    other words, the procedure is called on every item in the list
+    sequentially, but since each call has no return value, the call to
+    procmap() also has no return value.
+
+    @param procedure: The procedure to call each time.
+    @param itemList: The list of items to apply the procedure to.
+    @return: None
+    """
+    for item in itemList:
+        method(item)
+    
+    return
+
+#----------------------------------------------------------------------------#
+
+def tailEnumerate(sequence):
+    """
+    Same as enumerate(), but yields tuples of (item, index), so that you can
+    sort and otherwise use them nicely.
+    """
+    i = 0
+    for item in sequence:
+        yield (item, i)
+        i += 1
+
+    return
+
+#----------------------------------------------------------------------------#
+
+def separate(method, seq):
+    """
+    Uses the given method to separate the sequence into two lists, one for
+    which method(item) returns True for every item, the other for which
+    method(item) return False.
+
+    @param method: The boolean-valued function to separate with.
+    @type method: function
+    @param seq: A sequence of objects.
+    @type seq: sequence 
+    @return: A (trueList, falseList) tuple.
+    """
+    trueList = []
+    falseList = []
+
+    for item in seq:
+        if method(item):
+            trueList.append(item)
+        else:
+            falseList.append(item)
+
+    return (trueList, falseList)
+
+#----------------------------------------------------------------------------#
+
+def separateByClass(method, seq):
+    """
+    Distributes a sequence into a dictionary of classes, using the given
+    method's return value as the class label. This is a generalization of 
+    the separate() method.
+
+    @param method: The function generating class labels from items. 
+    @type method: function
+    @param seq: A sequence of objects.
+    @type seq: sequence
+    @return: A dictionary of class object lists.
+    """
+    results = {}
+    for item in seq:
+        key = method(item)
+        if key in results:
+            results[key].append(item)
+        else:
+            results[key] = [item]
+
+    return results
+
+#----------------------------------------------------------------------------#
+
+def head(n, seq):
+    """
+    Returns an iterator over the first n items in a sequence.
+    
+    If the sequence is itself an iterator, this will also advance the
+    iterator n places.
+
+        >>> head(3, range(10))
+        [0, 1, 2]
+
+        >>> x = iter(xrange(10))
+        >>> head(3, x)
+        [0, 1, 2]
+        >>> head(4, x)
+        [3, 4, 5, 6]
+        >>> head(100, x)
+        [7, 8, 9]
+    """
+    if hasattr(seq, '__getslice__'):
+        return seq[:n]
+
+    result = []
+    i = 0
+
+    # The empty case.
+    if n < 1:
+        return
+
+    for item in seq:
+        result.append(item)
+
+        i += 1
+        if i >= n:
+            break
+
+    return result
+
+#----------------------------------------------------------------------------#
+
+def ihead(n, seq):
+    """
+    Returns an iterator over the first n items in a sequence.
+    
+    If the sequence is itself an iterator, this will also advance the
+    iterator n places.
+
+        >>> list(ihead(3, xrange(10)))
+        [0, 1, 2]
+
+        >>> x = iter(xrange(10))
+        >>> list(ihead(3, x))
+        [0, 1, 2]
+        >>> list(ihead(4, x))
+        [3, 4, 5, 6]
+        >>> list(ihead(100, x))
+        [7, 8, 9]
+    """
+    i = 0
+
+    # The empty case.
+    if n < 1:
+        return
+
+    for item in seq:
+        yield item
+
+        i += 1
+        if i >= n:
+            break
+
+    return
+#----------------------------------------------------------------------------#
+
+def tail(n, seq):
+    """
+    Returns a list of the last n items in the iterator.
+
+    This is particularly efficient if n is much smaller than the length of the
+    iterator. It also requires O(n) storage.
+
+        >>> tail(3, xrange(10))
+        [7, 8, 9]
+
+        >>> tail(10, xrange(3))
+        [0, 1, 2]
+    """
+    output = n * [None]
+    i = 0
+
+    for item in seq:
+        output[i % n] = item
+        i += 1
+
+    if i <= n:
+        return output[:i]
+    else:
+        start = i % n
+        return output[start:] + output[:start]
+
+#----------------------------------------------------------------------------#
