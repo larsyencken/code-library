1af9d2f80c3a71baa4b5442df4f65184cf693d32
Lars Yencken <lljy@csse.unimelb.edu.au>
2009-08-20 09:45:41 +1000

    Improves haskell spell-checker example to use a temp file.

--- haskell/spell/spell.hs
+++ haskell/spell/spell.hs
@@ -5,14 +5,31 @@
 --
 
 import System.Environment
+import System.IO
+import System.Posix.Temp
 import Control.Monad
 import Data.Set
+import Data.Maybe
+import Text.Printf
 
 main = do
+    (f, g) <- readFiles
+    let dict = fromList (lines f)
+        errs = mapMaybe (spell dict) (words g)
+    write errs
+
+readFiles = do
     [s] <- getArgs
     f <- readFile "/usr/share/dict/words"
     g <- readFile s
-    let dict = fromList (lines f)
-    mapM_ (spell dict) (words g)
+    return (f, g)
 
-spell d w = when (w `notMember` d) (putStrLn w)
+write errs = do
+    (t, h) <- mkstemp "/tmp/spell.XXXXXX"
+    mapM_ (hPutStrLn h) errs
+    hClose h
+    printf "%d spelling errors written to '%s'\n" (length errs) t
+
+spell d w
+    | w `notMember` d   = Just w
+    | otherwise         = Nothing
