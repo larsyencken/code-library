b598eaa224d00b4f751effbfa6d3e11ec5ddce10
Lars Yencken <lljy@csse.unimelb.edu.au>
2009-08-20 10:21:37 +1000

    Parallelised the spell checking Haskell example.

--- haskell/spell/spell.hs
+++ haskell/spell/spell.hs
@@ -4,32 +4,54 @@
 -- Basic spellchecker. 
 --
 
-import System.Environment
-import System.IO
-import System.Posix.Temp
-import Control.Monad
-import Data.Set
+import Data.Set hiding (map)
 import Data.Maybe
+import Data.Char
 import Text.Printf
+import System.IO
+import System.Environment
+import Control.Monad
+import Control.Concurrent
 
 main = do
-    (f, g) <- readFiles
+    (f, g, n) <- readFiles
     let dict = fromList (lines f)
-        errs = mapMaybe (spell dict) (words g)
-    write errs
+        work = chunk n (words g)
+    run n dict work
+
+chunk n list = case list of { [] -> [] ; (y:ys) -> ch' ys (n-1) (y:) }
+    where
+    ch' [] _ k = k [] : []
+    ch' (y:ys) 0 k = k [] : ch' ys (n-1) (y:)
+    ch' (y:ys) (c+1) k = ch' ys c (k . (y:))
 
 readFiles = do
-    [s] <- getArgs
+    [s, n] <- getArgs
     f <- readFile "/usr/share/dict/words"
     g <- readFile s
-    return (f, g)
+    return (f, g, read n)
 
-write errs = do
-    (t, h) <- mkstemp "/tmp/spell.XXXXXX"
-    mapM_ (hPutStrLn h) errs
-    hClose h
-    printf "%d spelling errors written to '%s'\n" (length errs) t
+--write errs = do
+--    (t, h) <- mkstemp "/tmp/spell.XXXXXX"
+--    mapM_ (hPutStrLn h) errs
+--    hClose h
+--    printf "%d spelling errors written to '%s'\n" (length errs) t
 
-spell d w
-    | w `notMember` d   = Just w
-    | otherwise         = Nothing
+run n dict work = do
+    chan <- newChan
+    errs <- getChanContents chan
+    mapM_ (forkIO . thread chan dict) (zip [1..n] work)
+    wait n errs 0
+
+wait n xs i = when (i < n) $ case xs of
+    Nothing : ys -> wait n ys $! i+1
+    Just s  : ys -> putStrLn s >> wait n ys i
+
+thread chan dict (me, xs) = do
+    mapM_ spellit xs
+    writeChan chan Nothing
+
+    where
+        spellit w = when (w `notMember` dict) $
+            writeChan chan . Just $ printf "Thread %d: %-25s" (me::Int) w
+
