8520ac439b98fc3205c1b58080b931284e74e5f6
Lars Yencken <lljy@csse.unimelb.edu.au>
2010-05-15 23:03:00 +1000

    Updates the node http example to provide diagnostic printing.

--- javascript/node/http.js
+++ javascript/node/http.js
@@ -1,10 +1,12 @@
 // vim: ts=2 sw=2 sts=2 et :
 
 var http = require("http");
+var puts = require("sys").puts
 
 http.createServer(function (req, res) {
-    res.sendHeader(200, {"Content-Type": "text/plain"});
+    res.writeHead(200, {"Content-Type": "text/plain"});
     res.write("Hello\n");
     res.write("World\n");
     res.end();
   }).listen(8000);
+puts("Listening at http://localhost:8000/");
