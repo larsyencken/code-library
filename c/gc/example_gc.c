ef22250c3a414b5480c5d17d8f853ab338d1ac2a
Lars Yencken <lars@yencken.org>
2010-09-18 21:44:09 +1000

    Adds an example of garbage-collected C using BoehmGC.

---
+++ c/gc/example_gc.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdio.h>
+#include <gc.h>
+ 
+int main(void)
+{
+    int i;
+ 
+    GC_INIT();
+    for (i = 0; i < 10000000; ++i)
+    {
+        int **p = GC_MALLOC(sizeof(int *));
+        int *q = GC_MALLOC_ATOMIC(sizeof(int));
+ 
+        assert(*p == 0);
+        *p = GC_REALLOC(q, 2 * sizeof(int));
+        if (i % 100000 == 0)
+            printf("Heap size = %d\n", GC_get_heap_size());
+    }
+ 
+    return 0;
+}
+
