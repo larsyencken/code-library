ba767002a635558db18185a5e16d98518679559a
Lars Yencken <lars@yencken.org>
2010-11-25 17:55:59 +1100

    Fixes a minor bug and adds an example for GR chains.

--- python/gr/shortest_path.py
+++ python/gr/shortest_path.py
@@ -26,7 +26,12 @@ def shortest_path(vocab_file, gr_file):
     vocab = set(l.rstrip() for l in open(vocab_file))
     graph = _load_graph(gr_file, vocab)
     for sp in _find_shortest_paths(graph):
-        print sp
+        if len(sp.path) <= 9:
+            _print_path(sp)
+
+def _print_path(p):
+    print '1', '/'.join(['@'] + p.path + ['@'])
+    print '1 %s %s' % (p.start, p.end)
 
 def _load_graph(gr_file, vocab):
     parent = {}
@@ -53,7 +58,7 @@ def _parse_edge(gr_edge):
     parts = gr_edge.split()
     if len(parts) == 3:
         return parts
-    elif len(parts == 4):
+    elif len(parts) == 4:
         label = parts[0]
         if label.endswith('subj'):
             return parts[:3]
@@ -66,13 +71,12 @@ def _parse_edge(gr_edge):
 def _find_shortest_paths(graph):
     parent, conj, selected = graph
     for v_a, v_b in itertools.combinations(selected, 2):
-        import pdb; pdb.set_trace()
         if _nl(v_a) == _nl(v_b):
             continue
 
         if v_a in conj and conj.get(v_a) == graph.conj.get(v_b):
             label, v_conj = conj.get(v_a)
-            path = [_rev(label), _nl(v_conj), label]
+            path = ['[%s]' % _rev(label), _nl(v_conj), '[%s]' % label]
             yield Path(_nl(v_a), _nl(v_b), path)
 
         # XXX find least common subsumer
@@ -89,18 +93,21 @@ def _find_shortest_paths(graph):
             path_b.append(e)
 
         # trim the end
-        while path_a[-2] == path_b[-2] and path_a[-1] == path_b[-1]:
+        while len(path_a) > 1 and len(path_b) > 1 and path_a[-1] == path_b[-1]:
             path_a.pop()
             path_b.pop()
 
         path = []
         for e in path_a:
-            path.append("%s'" % e.label)
+            path.append("[%s']" % e.label)
             path.append(_nl(e.source))
 
+        first = True
         for e in reversed(path_b):
-            path.append("%s" % e.label)
-            path.append(_nl(e.source))
+            if not first:
+                path.append(_nl(e.source))
+            path.append("[%s]" % e.label)
+            first = False
 
         yield Path(_nl(v_a), _nl(v_b), path)
 
