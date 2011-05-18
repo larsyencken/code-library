8e91822100f0c1ea2397def72bb3a47ea95f41d7
Lars Yencken <lars@yencken.org>
2011-05-18 17:12:06 +1000

    Adds more complex type ordering and packing examples.

--- c++/typeSizes/typeSizes.cpp
+++ c++/typeSizes/typeSizes.cpp
@@ -7,20 +7,59 @@
 
 #include <iostream>
 #include <python2.5/Python.h>
+#include <string>
 
 using namespace std;
 
-typedef struct composite
-{
+struct Composite {
     char p;
     char* p_ptr;
-} Composite;
+};
 
-typedef struct __attribute__ ((__packed__)) packed
-{
+struct __attribute__ ((__packed__)) Packed {
     char p;
     char* p_ptr;
-} Packed;
+};
+
+struct Terrible {
+    char a;
+    double b;
+    int c;
+};
+struct LessTerrible {
+    double b;
+    char a;
+    int c;
+};
+struct __attribute__ ((__packed__)) TerriblePacked {
+    char a;
+    double b;
+    int c;
+};
+
+struct TaraA {
+    short a;
+    short b;
+    std::string c;
+};
+
+struct TaraB {
+    short a;
+    std::string c;
+    short b;
+};
+
+struct __attribute__ ((__packed__)) TaraAPacked {
+    short a;
+    short b;
+    std::string c;
+};
+
+struct __attribute__ ((__packed__)) TaraBPacked {
+    short a;
+    std::string c;
+    short b;
+};
 
 //--------------------------------------------------------------------------//
 // main():
@@ -37,6 +76,13 @@ int main(int argc, char *argv[])
     cout << "Py_UNICODE\t" << sizeof(Py_UNICODE) << endl;
     cout << "Composite\t" << sizeof(Composite) << endl;
     cout << "Packed\t\t" << sizeof(Packed) << endl;
+    cout << "TaraA\t\t" << sizeof(TaraA) << endl;
+    cout << "TaraAPacked\t\t" << sizeof(TaraAPacked) << endl;
+    cout << "TaraB\t\t" << sizeof(TaraB) << endl;
+    cout << "TaraBPacked\t\t" << sizeof(TaraBPacked) << endl;
+    cout << "Terrible\t\t" << sizeof(Terrible) << endl;
+    cout << "TerriblePacked\t\t" << sizeof(TerriblePacked) << endl;
+    cout << "LessTerrible\t\t" << sizeof(LessTerrible) << endl;
     return 0;
 }
 
