b863f715146514d519848e31e024306f5c7476cc
Lars Yencken <lars@yencken.org>
2012-02-17 20:35:48 +1100

    Euler 06: clojure.

---
+++ clojure/euler/06-sum-squares.clj
@@ -0,0 +1,34 @@
+#!/usr/bin/env clj
+;
+;  06-sum-squares.py
+;  Code
+;
+;  Created by Lars Yencken on 2012-02-17.
+;  Copyright 2012 99designs. All rights reserved.
+;
+
+(ns euler
+  (:use clojure.test))
+
+(defn sum-sq [xs]
+  (reduce + (map #(* % %) xs)))
+
+(deftest sum-sq-1
+  (is (= (sum-sq (range 1 4)) 14)))
+
+(defn sq-sum [xs]
+  (let [sum (reduce + xs)]
+    (* sum sum)))
+
+(deftest sq-sum-1
+  (is (= (sq-sum (range 1 4)) 36)))
+
+(deftest problem-test
+  (is (= (- (sq-sum (range 1 101)) (sum-sq (range 1 101))) 25164150)))
+
+(run-tests)
+(println)
+(println "Answer:"
+  (-
+    (sq-sum (range 1 101))
+    (sum-sq (range 1 101))))
