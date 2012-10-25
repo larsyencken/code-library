d8be4fa80561b8be7348fd6eb2297554f12eba93
Lars Yencken <lars@yencken.org>
2012-10-25 13:44:26 +1100

    Hello World web app in compojure.

---
+++ clojure/compojure/hello-world/project.clj
@@ -0,0 +1,9 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.1"]]
+  :plugins [[lein-ring "0.7.3"]]
+  :ring {:handler hello-world.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
